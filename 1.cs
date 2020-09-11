1662842925dff990d4f39aca8dbd533c67165358
Daniel Rayo (principal) <danirayoroldan@gmail.com>
2020-09-11 14:33:33 -0500

    Primer Ejercicios resuelto

--- 1.cs
+++ 1.cs
@@ -1,46 +1,39 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace variables
+namespace ejercicio1
 {
     class Program
     {  
         static void Main(string[] args)
         {
-            int num1;
-            int num2;
-            int numeroMax;
-
-            Console.WriteLine("¿Hasta que número quieres fijar el límite?");
-            numeroMax = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Ingresa primer número:");
-            num1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Ingresa segundo número:");
-            num2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine(muestraSuma(numeroMax,num1,num2));
-
+            Console.WriteLine(solucionOP(1000,3,5));
+            Console.WriteLine(sumaMultiplos(1000,3) + sumaMultiplos(1000,5));
         }
 
-        static int muestraSuma (int numeroMax, int num1, int num2)
-        {
-            int sumaPrimerNumero1 = sumaMultiplos(numeroMax,num1);
-            int sumaSegundoNumero2 =sumaMultiplos(numeroMax,num2);
-
-            int sumaTotal = sumaPrimerNumero1 + sumaSegundoNumero2;
-
-            return sumaTotal;
+        
+        // SOLUCIÓN MÁS OPTIMA
+        static int solucionOP(int numeroMax,int num1, int num2)
+        {   
+            int result = 0;
+            for (int i = 1; i < numeroMax; i++) 
+            {
+                if ((i % num1) == 0 || (i % num2) == 0) 
+                {
+                    result += i;
+                }
+            }
+            return result;
         }
-
+        
+        // SOLUCION INCORRECTA: obtiene los multiplos de los números dados correctamente, PERO no toma en cuenta que
+        //                      pueden haber multiplos en común, esos multiplos en común los suma dos veces afectando la respuesa.
         static int sumaMultiplos (int numeroMax, int numerobase)
         {
             int multiplo = 0;
             int sumaMultiplos = 0;
-            int i = 1;
 
-            while(multiplo<numeroMax)
+            for(int i = 1 ; multiplo<numeroMax;)
             {
                 // Para obtener un multiplo multiplicamos el número base por otros numero (i).
                 multiplo = numerobase*i;
@@ -50,7 +43,6 @@ namespace variables
                 // Para asegurar que el siguiente multiplo sea menor el numeroMax, y no entre en el ciclo.
                 multiplo = numerobase*i;
             }
-
             return sumaMultiplos;
         }
     }
