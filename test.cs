ef11b69a2a569abf259769b758fbba6c7c87f8d1
DaniLinux <danirayorolan@gmail.com>
2020-11-15 18:06:11 -0600

    ejercicio 8 en proceso

--- test.cs
+++ test.cs
@@ -1,16 +1,24 @@
 using System;
 using System.Diagnostics;
+using System.Text;
 namespace test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-			ejercicio4.Program.MiSolucion();
-            stopWatch.Stop(); 
-			Console.WriteLine(stopWatch.Elapsed.ToString());
-        }
+            // Stopwatch stopWatch = new Stopwatch();
+            // stopWatch.Start();
+            // stopWatch.Stop(); 
+			// Console.WriteLine(stopWatch.Elapsed.ToString());
+			var numero = new StringBuilder("123456789");
+			int producto = 0;
+			for(int i = 0; i<numero.Length; i++){
+				producto *= Convert.ToInt32(numero[i]);
+			}
+			Console.WriteLine(producto);
+			Console.WriteLine(numero[3]);
+
+		}
     }
 }
