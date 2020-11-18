49317e21ade992fad026edb061c78f8862dff2e0
DaniLinux <danirayorolan@gmail.com>
2020-11-18 17:35:04 -0600

    ejercicio 8 resuelto

--- test.cs
+++ test.cs
@@ -7,12 +7,18 @@ namespace test
     {
         static void Main(string[] args)
         {
-            // Stopwatch stopWatch = new Stopwatch();
-            // stopWatch.Start();
-            // stopWatch.Stop(); 
-			// Console.WriteLine(stopWatch.Elapsed.ToString());
-			ejercicio8.Program.mayorProducto();	
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+			ejercicio8.Program.test();	
+            stopWatch.Stop(); 
+			Console.WriteLine(stopWatch.Elapsed.ToString());
+			
+/*            Stopwatch stopWatch2 = new Stopwatch();
+            stopWatch2.Start();
 			ejercicio8.Program.test();
+            stopWatch2.Stop(); 
+			Console.WriteLine(stopWatch2.Elapsed.ToString());
+			*/
 		}
     }
 }
